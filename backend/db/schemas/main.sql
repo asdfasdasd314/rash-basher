d61c8b795e0f1d088f6620e0850622cce7c2823b
James Hollingsworth <Jamesholling2000@gmail.com>
2025-04-07 19:57:36 -0400

    Database setup

---
+++ backend/db/schemas/main.sql
@@ -0,0 +1,14 @@
+-- Set up our database
+CREATE TABLE users (
+    user_id TEXT PRIMARY KEY NOT NULL UNIQUE,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE diagnoses (
+    diagnosis_id TEXT PRIMARY KEY NOT NULL UNIQUE,
+    user_id TEXT NOT NULL,
+    diagnosis TEXT NOT NULL,
+    diagnosed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    image BLOB
+);
